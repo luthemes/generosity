6be9ec2e5f96ae0314036439a8f7c56d709f5f89
Benjamin Lu <164923145+techkodeninja@users.noreply.github.com>
2025-04-28 20:40:07 +0000

    initial commit

--- public/views/comments/default.php
+++ public/views/comments/default.php
@@ -1,16 +1,4 @@
 <?php
-/**
- * Default comments template
- *
- * @package   Initiator
- * @author    Benjamin Lu ( benlumia007@gmail.com )
- * @copyright Copyright (C) 2019-2021. Benjamin Lu
- * @license   https://www.gnu.org/licenses/gpl-2.0.html
- * @link      https://github.com/benlumia007/initiator
- */
-?>
-
-<?php
 if ( post_password_required() ) {
 	return;
 }
@@ -23,29 +11,28 @@ if ( post_password_required() ) {
 				<?php $count = get_comments_number(); ?>
 				<?php
 				if ( '1' === $count ) {
-					printf( esc_html_x( 'One Comment', 'comments title', 'generosity' ) );
+					printf( esc_html_x( 'One Comment', 'comments title', 'amicable' ) );
 				} else {
 					// Translators: 1 = counts.
-					printf( _nx( '%1$s Comment', '%1$s Comments', absint( $count ), 'comments title', 'generosity' ), absint( number_format_i18n( $count ) ) ); // phpcs:ignore
+					printf( _nx( '%1$s Comment', '%1$s Comments', absint( $count ), 'comments title', 'amicable' ), absint( number_format_i18n( $count ) ) ); // phpcs:ignore
 				}
 				?>
 			</h2>
 		<?php } ?>
 		<ol class="comment-list">
 			<?php
-			wp_list_comments(
-				array(
-					'style'       => 'ol',
-					'short_ping'  => true,
-					'avatar_size' => 70,
-				)
-			);
+			wp_list_comments( [
+				'avatar_size' => 60,
+				'callback'    => function( $comment, $args, $depth ) {
+					Backdrop\View\display( 'comment', Backdrop\Comment\hierarchy(), compact( 'comment', 'args', 'depth' ) );
+				}
+			] );
 			?>
 		</ol>
 		<?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) { ?>
 			<nav id="comment-nav-below" class="comment-navigation" role="navigation">
-				<div class="comment-previous"><?php previous_comments_link( '<i class="fa fa-arrow-circle-o-left"></i> ' . esc_html__( 'Older Comments', 'generosity' ) ); ?></div>
-				<div class="comment-next"><?php next_comments_link( '<i class="fa fa-arrow-circle-o-right"></i> ' . esc_html__( 'Newer Comments', 'generosity' ) ); ?></div>
+				<div class="comment-previous"><?php previous_comments_link( '<i class="fa fa-arrow-circle-o-left"></i> ' . esc_html__( 'Older Comments', 'amicable' ) ); ?></div>
+				<div class="comment-next"><?php next_comments_link( '<i class="fa fa-arrow-circle-o-right"></i> ' . esc_html__( 'Newer Comments', 'amicable' ) ); ?></div>
 			</nav>
 		<?php } ?>
 		<?php comment_form(); ?>

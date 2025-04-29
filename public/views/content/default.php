e2cb4d0b87d8d359849673ecebe046bbc1e0f9d3
Benjamin Lu <164923145+techkodeninja@users.noreply.github.com>
2025-04-29 23:48:26 +0000

    add global-layout theme mod.

--- public/views/content/default.php
+++ public/views/content/default.php
@@ -1,26 +1,17 @@
-<?php
-/**
- * Default default template
- *
- * @package   Succotash
- * @author    Benjamin Lu <benlumia007@gmail.com>
- * @copyright 2014-2022. Benjamin Lu
- * @license   https://www.gnu.org/licenses/gpl-2.0.html
- * @link      https://github.com/benlumia007/luthemes.com
- */
-?>
 <section id="content" class="site-content">
-	<main id="main" class="content-area">
-		<?php
-		if ( have_posts() ) :
-			while ( have_posts() ) : the_post();
-				Backdrop\Template\View\display( 'entry' );
-			endwhile;
-			the_posts_pagination();
-		else :
-			Backdrop\Template\View\display( 'entry/none' );
-		endif;
-		?>
-	</main>
-	<?php Backdrop\Template\View\display( 'sidebar', 'primary', [ 'location' => 'primary' ] ); ?>
+	<div id="global-layout" class="<?php echo esc_attr( Generosity\Tools\Mod::get( 'theme_content_layout' ) ); ?>">
+		<main id="main" class="content-area">
+			<?php
+			if ( have_posts() ) :
+				while ( have_posts() ) : the_post();
+					Backdrop\View\display( 'entry' );
+				endwhile;
+				the_posts_pagination();
+			else :
+				Backdrop\View\display( 'entry/none' );
+			endif;
+			?>
+		</main>
+		<?php Backdrop\View\display( 'sidebar', 'primary', [ 'location' => 'primary' ] ); ?>
+	</div>
 </section>

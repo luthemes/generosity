b53e9fa3daece9c82240789440f8d32971cebf89
Benjamin Lu <164923145+techkodeninja@users.noreply.github.com>
2025-04-29 23:25:15 +0000

    initial commit

---
+++ app/Layout/App/Layouts.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * Layouts Collection.
+ *
+ * Houses the collection of layouts in a single array-object.
+ *
+ * @package   Generosity
+ * @author    Benjamin Lu <benlumia007@gmail.com>
+ * @copyright 2025 Benjamin Lu
+ * @license   https://www.gnu.org/licenses/gpl-2.0.html
+ * @link      https://luthemes.com/portfolio/generosity
+ */
+
+namespace Generosity\Layout\App;
+
+use Generosity\Tools\Collection;
+
+/**
+ * Layouts class.
+ *
+ * @since  1.0.0
+ * @access public
+ */
+class Layouts extends Collection {
+
+	/**
+	 * Adds a new layout to the collection.
+	 *
+	 * @since  1.0.0
+	 * @access public
+	 * @param  string  $name
+	 * @param  array   $value
+	 * @return void
+	 */
+	public function add( $name, $value ) {
+
+		parent::add( $name, $value instanceof Layout ? $value : new Layout( $name, $value )
+		);
+	}
+
+	/**
+	 * Returns an array of the choices for the customizer control.
+	 *
+	 * @since  1.0.0
+	 * @access public
+	 * @return array
+	 */
+	public function customizeChoices() {
+
+		$choices = [];
+
+		foreach ( $this->all() as $layout ) {
+			$choices[ $layout->name() ] = $layout->label();
+		}
+
+		return $choices;
+	}
+}

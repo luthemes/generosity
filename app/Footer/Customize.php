41a7835aa87c8e1e79e5eb0269a729d261ad0086
Benjamin Lu <164923145+techkodeninja@users.noreply.github.com>
2025-05-01 18:20:04 +0000

    initial commit

---
+++ app/Footer/Customize.php
@@ -0,0 +1,113 @@
+<?php
+/**
+ * Footer customize class.
+ *
+ * Adds customizer elements for the footer component.
+ *
+ * @package   Generosity
+ * @author    Benjamin Lu <benlumia007@gmail.com>
+ * @copyright 2025 Benjamin Lu
+ * @license   https://www.gnu.org/licenses/gpl-2.0.html
+ * @link      https://luthemes.com/portfolio/generosity
+ */
+
+namespace Generosity\Footer;
+
+use WP_Customize_Manager;
+use Generosity\Customize\Customizable;
+use Generosity\Tools\Mod;
+
+use Exhale\Template\Footer as TemplateFooter;
+
+/**
+ * Footer customize class.
+ *
+ * @since 1.0.0
+ * @access public
+ */
+class Customize extends Customizable {
+
+	/**
+	 * Registers customizer sections.
+	 *
+	 * @since  1.0.0
+	 * @access public
+	 * @param  WP_Customize_Manager  $manager
+	 * @return void
+	 */
+	public function registerSections( WP_Customize_Manager $manager ) {
+
+		$manager->add_section( 'theme_footer_credit', [
+			'title' => esc_html__( 'Credit', 'amicable' ),
+			'panel' => 'theme_footer'
+		] );
+	}
+
+	/**
+	 * Registers customizer settings.
+	 *
+	 * @since 1.0.0
+	 * @access public
+	 * @param  WP_Customize_Manager  $manager
+	 * @return void
+	 */
+	public function registerSettings( WP_Customize_Manager $manager ) {
+
+		// Register footer settings.
+		$manager->add_setting( 'theme_footer_powered_by', [
+			'default'           => Mod::fallback( 'theme_footer_powered_by' ),
+			'sanitize_callback' => 'wp_validate_boolean',
+            'transport'         => 'postMessage',
+		] );
+
+		$manager->add_setting( 'theme_footer_custom_credit', [
+			// Translators: %s is the theme link. 
+			'default'           => Mod::fallback( 'theme_footer_custom_credit' ),
+            'transport'         => 'postMessage',
+			'sanitize_callback' => function( $value ) {
+				return wp_kses( $value, Footer::allowedTags() );
+			},
+		] );
+	}
+
+	/**
+	 * Registers customizer controls.
+	 *
+	 * @since 1.0.0
+	 * @access public
+	 * @param  WP_Customize_Manager  $manager
+	 * @return void
+	 */
+	public function registerControls( WP_Customize_Manager $manager ) {
+
+
+		// Powered by control.
+		$manager->add_control( 'theme_footer_powered_by', [
+			'section'  => 'theme_footer_credit',
+			'type'     => 'checkbox',
+			'label'    => __( 'Show random "powered by" credit text.', 'amicable' ),
+		] );
+
+    		// Footer credit control.
+		$manager->add_control( 'theme_footer_custom_credit', [
+			'section'         => 'theme_footer_credit',
+			'type'            => 'textarea',
+			'label'           => __( 'Custom Footer Text', 'amicable' ),
+			'active_callback' => function( $control ) {
+				return ! $control->manager->get_setting( 'theme_footer_powered_by' )->value();
+			}
+		] );
+	}
+
+	/**
+	 * Registers customizer partials.
+	 *
+	 * @since 1.0.0
+	 * @access public
+	 * @param  WP_Customize_Manager  $manager
+	 * @return void
+	 */
+	public function registerPartials( WP_Customize_Manager $manager ) {
+
+	}
+}
---
+++ app/Footer/Provider.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Footer service provider.
+ *
+ * Bootstraps the Footer component.
+ *
+ * @package   Exhale
+ * @author    Justin Tadlock <justintadlock@gmail.com>
+ * @copyright 2019 Justin Tadlock
+ * @license   https://www.gnu.org/licenses/gpl-2.0.html GPL-2.0-or-later
+ * @link      https://themehybrid.com/themes/exhale
+ */
+
+namespace Generosity\Footer;
+
+use Backdrop\Core\ServiceProvider;
+
+/**
+ * Footer service provider class.
+ *
+ * @since  1.0.0
+ * @access public
+ */
+class Provider extends ServiceProvider {
+
+	/**
+	 * Binds components to the container.
+	 *
+	 * @since  1.0.0
+	 * @access public
+	 * @return void
+	 */
+	public function register() {
+		$this->app->singleton( Customize::class );
+	}
+}
